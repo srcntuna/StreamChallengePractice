9df0c50e71d46c60c993c50510e9f73066f9c949
Jay M <jaymansmann@yahoo.com>
2022-07-20 15:35:39 -0400

    Updated comment

--- untitled/src/main/java/org/example/statistics/TextStatisticsService.java
+++ untitled/src/main/java/org/example/statistics/TextStatisticsService.java
@@ -4,6 +4,8 @@ import org.example.statistics.model.TextStatistics;
 
 public class TextStatisticsService {
     public TextStatistics getStatistics(String text) {
+        // TODO
+        // Please use Streams
         return null;
     }
 }
